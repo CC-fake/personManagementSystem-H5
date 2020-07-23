2ff590190f546a1c361fd92a5959f4a16e551743
C.C <zhuyuchen00@163.com>
2020-07-23 21:50:58 +0800

    前端界面

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,24 @@
 <template>
-    <h2>欢迎使用人事管理系统</h2>
+    <div>
+        <h2>欢迎使用人事管理系统</h2>
+
+        <el-button type="primary" @click="open">打开</el-button>
+    </div>
+
+
 </template>
 
 <script>
     export default {
-        name: "home"
+        name: "home",
+        methods:{
+            open(){
+                const _this = this
+                axios.post("http://192.168.0.139:8080/device/open").then(function () {
+                    _this.$message('打开成功');
+                })
+            }
+        }
     }
 </script>
 
